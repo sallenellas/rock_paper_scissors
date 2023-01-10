310cd1ec643c803796f34eca93280f84307f9b0b
Samuel <sallenellas@gmail.com>
2023-01-10 07:33:07 -0600

    Added game functionality

--- index.js
+++ index.js
@@ -5,16 +5,70 @@ const variables = ['rock', 'paper', 'scissors']
 let userChoice = prompt("Type 'R' for rock, 'P' for paper, or 'S' scissors?").toLowerCase();
 
 //Testing If User Input Is Captured Properly
+// if (userChoice === 'r'){
+//     alert("0")
+// }
+// else if (userChoice === 'p'){
+//     alert("1")
+// }
+// else if (userChoice === 's'){
+//     alert("2")
+// }
+// else{
+//     alert("Invalid Input")
+// };
+
+//Computer Choice Function
+function randomChoice(choices) {
+    let index = Math.floor(Math.random() * choices.length)
+    return choices[index][0];
+}
+
+computerChoice = randomChoice(variables);
+
+console.log(computerChoice);
+
+// Tracking Wins/Losses
+let wins = 0
+let losses = 0
+
+// Game Functionality
 if (userChoice === 'r'){
-    alert("0")
+    if (computerChoice === 'p'){
+        alert('You Lose')
+        losses += 1
+    }
+    else if (computerChoice = 's'){
+        alert('You Win')
+        wins += 1
+    }
+    else{
+        alert("It's a Draw")
+    }
 }
 else if (userChoice === 'p'){
-    alert("1")
+    if (computerChoice === 's'){
+        alert('You Lose')
+        losses += 1
+    }
+    else if (computerChoice = 'r'){
+        alert('You Win')
+        wins += 1
+    }
+    else{
+        alert("It's a Draw")
+    }
 }
 else if (userChoice === 's'){
-    alert("2")
+    if (computerChoice === 'r'){
+        alert('You Lose')
+        losses += 1
+    }
+    else if (computerChoice = 'p'){
+        alert('You Win')
+        wins += 1
+    }
+    else{
+        alert("It's a Draw")
+    }
 }
-else{
-    alert("Invalid Input")
-};
-
