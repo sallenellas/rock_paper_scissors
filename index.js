ae60e32ddf954827c1069f75b0723365f0225c81
Samuel <sallenellas@gmail.com>
2023-05-02 19:15:18 -0500

    Javascript code cleaned up a bit and comments added

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const computerScore = document.querySelector('.computerScore');
 const pChoice = document.querySelector('.playerChoice');
 const cChoice = document.querySelector('.computerChoice');
 
+
+// keeps track of score
 var playerPoints = 0;
 var computerPoints = 0;
 
+
+//passes choices in and decides winner
 const scoreboard = (playerChoice, computerChoice) => {
 
     switch (playerChoice){
@@ -58,53 +62,19 @@ const scoreboard = (playerChoice, computerChoice) => {
 
 }
 
+// random choice generator for computer
 const getComputerChoice = () => {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 
+// round of game initiated with click of one of the buttons
 buttons.addEventListener('click', (e) => {
     let player = e.target.value;
     console.log(player);
     let computer = getComputerChoice();
     console.log(computer);
     scoreboard(player, computer);
-
-    // switch (playerChoice){
-    //     case('rock'):
-    //     if (computerChoice === 'rock'){
-    //         results.innerText = "It's A Tie";
-    //     }
-    //     else if (computerChoice === 'paper'){
-    //         results.innerText = `You Lose! ${computerChoice.toUpperCase()} beats ${playerChoice.toUpperCase()}`;
-    //     }
-    //     else{
-    //         results.innerText = `You Win! ${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
-    //     }
-    //     break;
-    //     case('paper'):
-    //     if (computerChoice === 'paper'){
-    //         results.innerText = "It's A Tie";
-    //     }
-    //     else if (computerChoice === 'scissors'){
-    //         results.innerText = `You Lose! ${computerChoice.toUpperCase()} beats ${playerChoice.toUpperCase()}`;
-    //     }
-    //     else{
-    //         results.innerText = `You Win! ${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
-    //     }
-    //     break;
-    //     case('scissors'):
-    //     if (computerChoice === 'scissors'){
-    //         results.innerText = "It's A Tie";
-    //     }
-    //     else if (computerChoice === 'rock'){
-    //         results.innerText = `You Lose! ${computerChoice.toUpperCase()} beats ${playerChoice.toUpperCase()}`;
-    //     }
-    //     else{
-    //         results.innerText = `You Win! ${playerChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
-    //     }
-    //     break;
-        
     
 });
